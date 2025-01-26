d8b9cdfe7b4953701b18d60d160aaec3d83cee2f
FES001 <firdavs7794948@gmail.com>
2025-01-26 21:20:07 +0500

    Working on it.

---
+++ script.js
@@ -0,0 +1,44 @@
+const box = document.querySelector('.box');
+const boxDragZone = document.querySelector('.box-controls-drag');
+const boxExpandButton = document.querySelector('.box-controls-expand');
+const mouseY = document.getElementById('mouseY');
+const mouseX = document.getElementById('mouseX');
+const boxOffsetTop = document.getElementById('boxOffsetTop');
+const boxOffsetLeft = document.getElementById('boxOffsetLeft');
+let posX = 0, posY = 0, newPosX = 0, newPosY = 0;
+
+// MOUSE Y AND MOUSE X
+window.addEventListener('mousemove', e => {
+    mouseY.textContent = e.clientY;;
+    mouseX.textContent = e.clientX;;
+});
+
+// DRAG AN ELEMENT
+boxDragZone.addEventListener('mousedown', e => {
+    posX = e.clientX;
+    posY = e.clientY;
+
+    box.style.transition = 'none';
+    document.addEventListener('mousemove', moveTheBox);
+    document.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', moveTheBox);
+        box.style.transition = '300ms';
+    });
+});
+
+function moveTheBox(e) {
+    newPosY = e.clientY - posY;
+    newPosX = e.clientX - posX;
+
+    box.style.top = newPosY + 'px';
+    box.style.left = newPosX + 'px';
+};
+
+// BOX EXPAND
+
+function boxExpand(e) { 
+    box.classList.toggle('box-js');
+};
+
+// INITIALIZING BUTTONS
+boxExpandButton.addEventListener('click', boxExpand);
